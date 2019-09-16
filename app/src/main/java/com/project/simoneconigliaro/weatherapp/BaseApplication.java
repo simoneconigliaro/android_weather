e213d49d7a11e2b00a90916c36d347e59682acb6
Simone Conigliaro <33692787+simoneconigliaro@users.noreply.github.com>
2019-09-16 19:54:06 +0100

    BaseApplication and AppComponent implemented

---
+++ app/src/main/java/com/project/simoneconigliaro/weatherapp/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.project.simoneconigliaro.weatherapp;
+
+import com.project.simoneconigliaro.weatherapp.di.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
+}
---
+++ app/src/main/java/com/project/simoneconigliaro/weatherapp/di/AppComponent.java
@@ -0,0 +1,27 @@
+package com.project.simoneconigliaro.weatherapp.di;
+
+import android.app.Application;
+
+import com.project.simoneconigliaro.weatherapp.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class,
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+}
